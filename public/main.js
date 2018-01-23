cb0f07b4de2e56b9fb592ba0c5c3b227eee2a59f
Shir Bukchin <shirbukchin@Shirs-MacBook-Air.local>
2018-01-23 11:45:15 +0200

    changes tast 3

--- public/main.js
+++ public/main.js
@@ -1,61 +1,69 @@
+var recipesApp = function () {
+  var recipes = [];
 
-var recipes = [];
+  // searh for recipes form:
+  var $recipeList = $(".recipe-list");
 
-// searh for recipes form:
-var $recipes = $(".recipes");
-
-function findRecipe() {
+  function findRecipe(text) {
+    var url = 'recipes?recipe=' + text;
     $.ajax({
       method: "GET",
-      dataType: 'json',
-     //url: ‘/recipes?recipe=<user input value>’, 
-      success: function(data) {
+      url: url,
+      success: function (data) {
         console.log(data);
         recipes = data;
-        _renderPage();
+        // _renderPage();
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
-    }
-}); 
-  }
-  findRecipe();
-
-  // click button "get recipes": 
-  $(".mainBtn").on('click', function() {
-    var $input = $(".mainInput");
-    if ($input.val() === "") {
-      alert("Please enter text");
-    }
-    else {
-     // app.addRecipe($input.val());
-     $input.val("");
-    };
-  });
-
-  function _renderPage() {
-    $recipes.empty();
-    var source = $('recipe-template').html();
-    var template = Handlebars.compile(source);
-    for (var i = 0; i <recipes.length; i++) {
-      var newHTML = template(recipes[i]);
-      console.log(newHTML);
-      $recipes.append(newHTML);
-      _renderPage(i);
-    }
-  }
-
-  function addRecipe(newRecipe) {
-    $.ajax({
-      type: "POST",
-      dataType: "json",
-      // url: '', // from server
-      data: {
-        text: newRecipe,
-      },
-      success: function(data) {
-        recipes.push(data);
-        _renderPage();
       }
     });
   }
+  // function _renderPage() {
+  //   $recipeList.empty();
+  //   var source = $('#recipe-template').html();
+  //   var template = Handlebars.compile(source);
+  //   for (var i = 0; i < recipes.length; i++) {
+  //     var newHTML = template(recipes[i]);
+  //     console.log(newHTML);
+  //     $recipeList.append(newHTML);
+  //   }
+  // }
+
+  // function addRecipe(newRecipe) {
+  //   $.ajax({
+  //     type: "POST",
+  //     dataType: "json",
+  //     // url: '', // from server
+  //     data: {
+  //       text: newRecipe,
+  //     },
+  //     success: function (data) {
+  //       recipes.push(data);
+  //       _renderPage();
+  //     }
+  //   });
+  // }
+
+  return {
+    findRecipe: findRecipe
+    // addRecipe: addRecipe
+
+  }
+
+}
+
+var app = recipesApp();
+
+// click button "get recipes": 
+$(".main-btn").on('click', function () {
+  var $input = $(".mainInput");
+  if ($input.val() === "") {
+    alert("Please enter text");
+  }
+  else {
+    app.findRecipe($input.val());
+    $input.val("");
+  }
+})
+
